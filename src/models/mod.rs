4fdd39ce5ac57dc49c132307063fff081d8bac86
Revanthshalon <revanthshalon@gmail.com>
2024-08-26 10:01:02 +0530

    User Data Access Objects Added

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,5 @@
 mod role;
+mod user;
 
 pub use role::{CreateRole, RoleListResponse, RoleResponse, UpdateRole};
+pub use user::{CreateUser, UpdateUser, UserListResponse, UserResponse};
---
+++ src/models/user.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateUser {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub role_id: Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateUser {
+    pub id: Uuid,
+    pub username: Option<String>,
+    pub email: Option<String>,
+    pub password: Option<String>,
+    pub role_id: Option<Uuid>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserResponse {
+    pub id: Uuid,
+    pub username: String,
+    pub email: String,
+    pub role_id: Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserListResponse {
+    pub users: Vec<UserResponse>,
+}

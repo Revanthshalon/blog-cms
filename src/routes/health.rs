38454c0daa2823e1c4123a123bd5ab96ec399efc
Revanthshalon <revanthshalon@gmail.com>
2024-08-26 09:54:38 +0530

    Health Check Route Path Correction

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -3,5 +3,5 @@ use axum::{routing::get, Router};
 use crate::handlers::check_app_health;
 
 pub fn create_health_routes() -> Router {
-    Router::new().route("/health", get(check_app_health))
+    Router::new().route("/", get(check_app_health))
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 use health::create_health_routes;
+use role::create_role_routes;
 
 use crate::services::ServiceContainer;
 
@@ -7,7 +8,7 @@ mod health;
 mod role;
 
 pub fn create_api_routes(services: ServiceContainer) -> Router {
-    let role_routes = Router::new().nest("/role", role::create_role_routes(services.clone()));
+    let role_routes = Router::new().nest("/role", create_role_routes(services.clone()));
     let health_routes = Router::new().nest("/health", create_health_routes());
     let merged_routes = Router::new().merge(role_routes).merge(health_routes);
     Router::new().nest("/api", merged_routes)
